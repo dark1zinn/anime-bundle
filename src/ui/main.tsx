9aa58eab742fea0bcaf14ff87a46cb4bd7abfc79
dark1zinn <edilsonjuininho154@gmail.com>
2024-11-28 20:03:50 -0300

    moved route to it's own file

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,46 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
 import './index.css'
-import App from './App.tsx'
-
-// router config
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-// pages
-import Home from './routes/Home.tsx'
-import About from './routes/About.tsx'
-import ErrorPage from './routes/ErrorPage.tsx'
-import Animes from './routes/Animes.tsx'
-import MediaProfile from './routes/MediaProfile.tsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: 'animes',
-        element: <Animes />
-      },
-      {
-        path: 'animes/:name',
-        element: <MediaProfile />
-      }
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router  } />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/ui/router.tsx
@@ -0,0 +1,39 @@
+
+// router config
+import { createBrowserRouter } from 'react-router-dom'
+
+// pages
+import Home from './routes/Home.tsx'
+import About from './routes/About.tsx'
+import ErrorPage from './routes/ErrorPage.tsx'
+import Animes from './routes/Animes.tsx'
+import MediaProfile from './routes/MediaProfile.tsx'
+import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: 'animes',
+        element: <Animes />
+      },
+      {
+        path: 'animes/:name',
+        element: <MediaProfile />
+      }
+    ],
+  },
+]);
+
+export default router
